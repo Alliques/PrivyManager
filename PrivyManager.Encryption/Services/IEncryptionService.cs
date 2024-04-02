2d764d970a4288d75d47c40e77f46881c42460b1
Aliques <ayrat.mt@gmail.com>
2024-04-02 22:34:46 +0300

    Context menu styles, refactoring button styles

---
+++ PrivyManager.Encryption/Services/IEncryptionService.cs
@@ -0,0 +1,10 @@
+﻿namespace PrivyManager.Encryption.Services
+{
+    public interface IEncryptionService
+    {
+        byte[] Encrypt(byte[] data, byte[] key);
+        byte[] Decrypt(byte[] encryptedData, byte[] key);
+        byte[] ComputeHash(byte[] data);
+        string ComputeHashString(byte[] data);
+    }
+}
